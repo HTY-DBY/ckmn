732283a8e0eed92ff272ef53717dbed24357dfc8
HTY-DBY <hty@hty.ink>
2024-06-17 14:49:37 +0800

    1

---
+++ bag_manage/Lottery/ckmn_Lottery_test/ckmn.js
@@ -0,0 +1,11 @@
+// ckmn.js
+module.exports = {
+  icon: 'https://i0.hdslb.com/bfs/face/946b4fecc2ad094b809f8e275353e67f799ad9af.jpg@240w_240h_1c_1s_!web-avatar-nav.avif',
+  name: '测试用 抽卡包',
+  About: '这是关于这个包的简要说明',
+  author: 'hty',
+  version: '1.0.0',
+  project_address: 'https://github.com/HTY-DBY',
+  update_address: 'https://www.bilibili.com',
+
+};
---
+++ bag_manage/Lottery/ckmn_genshin_hty/ckmn.js
@@ -0,0 +1,11 @@
+// ckmn.js
+module.exports = {
+  icon: 'https://i2.hdslb.com/bfs/face/853c4a846793dfbdafd8f16a40d6d8065bdb7c6f.jpg@240w_240h_1c_1s_!web-avatar-search-videos.avif',
+  name: '原神-ckmn',
+  About: '这是作者开发的原神抽卡包',
+  author: 'hty',
+  version: '1.0.0',
+  project_address: 'https://github.com/HTY-DBYssssssssssssssssssssssssssssssssssssssssssssssss',
+  update_address: 'https://www.bilibili.com',
+
+};
